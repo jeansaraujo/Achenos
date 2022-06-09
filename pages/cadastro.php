2313caf5b6bd01c3c1d3893a251b7c3875b6bc99
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-06-09 11:36:05 -0300

    Aula 09-06-2022 parte 02

--- pages/cadastro.php
+++ pages/cadastro.php
@@ -1,16 +1,37 @@
 <div class="row">
     <div class="col">
-        <p class="h2">Cadastro de Prestadores</p>
+        <p class="h2 text-end p-3 text-primary">Cadastro de Prestadores</p>
     </div>
 </div>
 <div class="row">
-    <div class="col-6 justify-content-center mt-1">
-        <form action="">
-            <input type="text" class="form-control" name="nome" placeholder="Nome">
-            <input type="email" class="form-control" name="email" placeholder="Email">
-            <input type="password" class="form-control" name="senha" placeholder="Senha">
-            <input type="text" class="form-control" name="servico" placeholder="Serviço">
-            <input type="tel" class="form-control" name="contato" placeholder="Contato">
+    <div class="offset-3 col-6 justify-content-center mt-1">
+        <form method="POST">
+            <input type="text" class="form-control" name="nome" placeholder="Nome" required>
+            <input type="email" class="form-control" name="email" placeholder="Email" required>
+            <input type="password" class="form-control" name="senha" placeholder="Senha" required>
+            <input type="text" class="form-control" name="servico" placeholder="Serviço" required>
+            <input type="tel" class="form-control" name="contato" placeholder="Contato" required>
+            <button class="btn btn-outline-primary btn-lg mt-2">
+                Cadastrar
+            </button>
         </form>
     </div>
 </div>
+
+<?php
+    if(isset($_POST['nome'])&isset($_POST['email'])&isset($_POST['senha'])&isset($_POST['servico'])&isset($_POST['contato'])){
+        $conexao = new PDO("mysql:dbname=senac;host=localhost","root","");
+        $insere = $conexao->prepare("INSERT INTO prestador(nome,email,senha,servico,contato) VALUES(:NOME,:EMAIL,:SENHA,:SERVICO,:CONTATO)");
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        $servico = $_POST['servico'];
+        $contato = $_POST['contato'];       
+        $insere->bindParam(":NOME",$nome);
+        $insere->bindParam(":EMAIL",$email);
+        $insere->bindParam(":SENHA",$senha);
+        $insere->bindParam(":SERVICO",$servico);
+        $insere->bindParam(":CONTATO",$contato);
+        $insere->execute();        
+    }
+?>
