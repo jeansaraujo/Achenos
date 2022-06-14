5fbca73fb870e736bc127e6a297a31c97a7b0af2
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-06-14 12:03:28 -0300

    Aula 14-06-2022 (parte 02)

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,60 @@
-<h1>
-    Home
-</h1>
+<style>
+    .carousel-item{
+        height:400px;
+    }
+</style>
+
+<div class="row">
+    <div class="col">
+        <p class="h2 text-end text-primary mt-3">Achenos <sup>Aqui, você acha seu Profissional</sup></p>
+    </div>
+</div>
+<div class="row">
+    <div class="col">        
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="https://static.ferramentaskennedy.com.br/storage/assets/pedreiro-saiba-mais-sobre-o-trabalho-deste-profissionalba23340cd5e226f57c57578c1245d811.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-primary h1">Pedreiro</h5>
+                    <p class="h4 bg-warning">Construa, reforme </p>
+                </div>
+                </div>
+                <div class="carousel-item">
+                <img src="https://www.ro.senac.br/wp-content/uploads/2020/10/cozinheiro.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-primary h1">Cozinheiro</h5>
+                    <p class="h4 bg-warning">Come bem, sem se preocupar ...</p>
+                </div>
+                </div>
+                <div class="carousel-item">
+                <img src="https://conteudo.imguol.com.br/c/entretenimento/21/2018/02/17/faxina-produto-de-limpeza-1518878154239_v2_615x300.jpg" class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5 class="text-primary h1">Faxina</h5>
+                    <p class="h4 bg-warning">Limpezas domésticas</p>
+                </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <div class="alert alert-warning h3 text-center" role="alert">
+           O serviço que precisa, nós temos para você!
+        </div>
+    </div>
+</div>
--- pages/login.php
+++ pages/login.php
@@ -1,3 +1,38 @@
-<h1>
-    Login
-</h1>
+<style>
+    .caixa{
+        border: 5px solid #0d6efd;
+        padding: 10px;
+        border-radius:20px;
+    }
+</style>
+<div class="row">
+    <div class="col">
+        <p class="h2 text-primary text-end mt-3">Área Administrativa</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col">
+        <form method="POST">
+            <div class="row">
+                <div class="offset-4 col-4 offset-4 caixa">                    
+                    <input class="form-control" type="email" name="email" placeholder="Email">
+                    <input class="form-control mt-2" type="password" name="senha" placeholder="Senha">
+                    <button class="btn btn-outline-primary mt-2" type="submit">
+                        Acessar
+                    </button>                
+                </div>
+            </div>    
+        </form>
+    </div>
+</div>
+
+<?php
+    if(isset($_POST['email'])&isset($_POST['senha'])){
+        $email = $_POST['email'];
+        $senha = $_POST['senha'];
+        if($senha=="123"){
+            header("Location:http://www.google.com");
+        }       
+
+    }
+?>
