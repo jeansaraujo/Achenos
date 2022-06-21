183bef58620460ca0b4b92c88466de2db11342d8
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-06-21 12:03:59 -0300

    Aula 21-06-2022

--- admin/index.php
+++ admin/index.php
@@ -1,44 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <title>Administração</title>
 </head>
-<style>
-    #menu-lateral{
-        height:400px;
-    }
-</style>
 <body>
-    <div class="container">
-        <div class="row bg-primary">
-            <div class="col-2">
-                <p class="h2">                
+    <header class="container-fluid bg-primary p-2">
+        <div class="row">
+            <div class="col-2 d-flex justify-content-center align-items-center">
+                <p class="h3 text-white">
                     Achenos
                 </p>
             </div>
-            <div class="col-8 d-flex justify-content-center     align-items-center"> 
-                <p class="h5">
-                    Administração    
-                </p>    
+            <div class="col-8 text-center h4 text-white">
+                Área Administrativa
             </div>
-            <div class="col-2">Usuário</div>
-        </div>
-        <div class="row" id="menu-lateral">
-            <div class="col-3 bg-warning d-flex flex-column justify-content-around">
-                <a href="" class="btn btn-outline-primary">Menu</a><a href="" class="btn btn-outline-primary">Menu</a><a href="" class="btn btn-outline-primary">Menu</a><a href="" class="btn btn-outline-primary">Menu</a><a href="" class="btn btn-outline-primary">Menu</a>
-            </div>
-            <div class="col-9 bg-danger">
-
+            <div class="col-2">
+                <a href="/programador/achenos2/" class="btn btn-outline-light">Sair</a>
             </div>
         </div>
+    </header>
+    <style>
+        #lateral{
+            height:80vh;
+        }
+        footer{
+            height:10vh;
+        }
+    </style>
+    <main class="container bg-light">
         <div class="row">
-            <div class="col bg-dark text-center text-white">Rodapé</div>
-        </div>        
-    </div>    
+            <div class="col-2 d-flex flex-column justify-content-around" id="lateral">
+                <a href="" class="btn btn-outline-primary">Home</a>
+                <a href="" class="btn btn-outline-primary">Prestador</a>
+                <a href="" class="btn btn-outline-primary">Serviços</a>
+                <a href="" class="btn btn-outline-primary">Slider</a>
+                <a href="" class="btn btn-outline-primary">Categorias</a>
+            </div>
+            <div class="col-10"></div>
+        </div>
+    </main>
+    <footer class="container-fluid bg-dark">
+        <div class="row text-white">
+            <div class="col d-flex flex-column justify-content-center">
+                <p class="text-center">
+                   Programador de Sistemas
+                </p>                
+            </div>
+            <div class="col d-flex justify-content-between mt-3">
+                <a href="" class="btn btn-outline-light">infos 1</a>
+                <a href="" class="btn btn-outline-light">infos 2</a>
+                <a href="" class="btn btn-outline-light">infos 3</a>                
+            </div>
+        </div>   
+    </footer>
 </body>
 </html>
--- pages/login.php
+++ pages/login.php
@@ -1,32 +1,3 @@
-<!--
-<style>
-    .caixa{
-        border: 5px solid #0d6efd;
-        padding: 10px;
-        border-radius:20px;
-    }
-</style>
-<div class="row">
-    <div class="col">
-        <p class="h2 text-primary text-end mt-3">Área Administrativa</p>
-    </div>
-</div>
-<div class="row">
-    <div class="col">
-        <form method="POST">
-            <div class="row">
-                <div class="offset-4 col-4 offset-4 caixa">                    
-                    <input class="form-control" type="email" name="email" placeholder="Email">
-                    <input class="form-control mt-2" type="password" name="senha" placeholder="Senha">
-                    <button class="btn btn-outline-primary mt-2" type="submit">
-                        Acessar
-                    </button>                
-                </div>
-            </div>    
-        </form>
-    </div>
-</div>
--->
 <div class="row">
     <div class="col">
         <p class="text-primary h2 text-end mt-2">
@@ -56,32 +27,23 @@
         </form>
     </div>
 </div>
-
-
 <?php
-    if(isset($_POST['email'])&isset($_POST['senha'])){
-        $conexao = new PDO("mysql:dbname=senac;host=localhost","root","");
-        $consulta = $conexao->prepare("SELECT senha FROM prestador WHERE email = :EMAIL");
-        $email = $_POST['email'];        
-        $consulta->bindParam(":EMAIL",$email);
-        $consulta->execute();
-        $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
-        $senhausuario="ui";        
-        foreach($resultado as $r){
-            $senhausuario = $r['senha'];
-        }
-        $senha=$_POST['senha'];
-        if($senhausuario=="ui"){
-            echo "<strong>".$email." </strong> não está cadastrado";
-        }
-        else{
-            if($senhausuario==$senha){
-                header("Location:admin");
-            }
-            else{
-                echo "A Senha <strong>". $senha. " </strong> está incorreta";
-            }
-        }
+   if(isset($_POST['email'])&isset($_POST['senha'])){
+    $conexao = new PDO("mysql:dbname=senac;host=localhost","root","");
+    $consulta = $conexao->prepare("SELECT senha FROM prestador WHERE email=:EMAIL");
+    $email=$_POST['email'];
+    $consulta->bindParam(":EMAIL",$email);
+    $consulta->execute();
+    $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);   
+    if($resultado[0]["senha"]==$_POST['senha']){
+        header("Location:admin");
     }
-?>      
-
+    else{
+        echo "
+        <div class='alert alert-danger text-center mt-3' role='alert'>
+            Usuário ou Senha Inválido!
+        </div>
+        ";
+    }
+   }
+?>
