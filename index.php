52284907bb09b1f408ae4ba79ae56f7dbbb2e1cc
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-07-12 11:16:26 -0300

    Ajuste no header

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
     include_once 'config/config.php'; 
     include_once 'lib/handling.php'; 
     include_once 'lib/Session.php'; 
-    include_once 'lib/queryhandling.php';
-    include_once 'include/header.php';
+    include_once 'lib/queryhandling.php';    
     Session::init(); 
     if (isset($_GET['action']) && $_GET['action'] == 'logout') {
         Session::destroy();
@@ -21,80 +20,18 @@
     <script src="assets/js/script.js"></script>
     <title>Achenos - O profissional que precisa</title>
     <style>
- footer {
-
-text-align: center;
-padding: 20px;
-background-color: #00407d;
-color: white;
-}               
-</style>
+        footer {
+            text-align: center;
+            padding: 20px;
+            background-color: #00407d;
+            color: white;
+        }               
+    </style>
 </head>
 <body>
-    <header class="container-fluid">
-        <div class="logo d-flex justify-content-center">
-            <img src="assets/img/logo_achenos.jpg">
-        </div>
-        <nav class="navbar navbar-expand-lg bg-light">
-          <div class="container">
-              <div class="row">
-                  <div class="offset-1 col-10 offset-1">
-                      <a class="navbar-brand" href="#"></a>
-                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                          <span class="navbar-toggler-icon"></span>
-                      </button>
-                      <div class="collapse navbar-collapse" id="navbarNav">
-                          <ul class="d-flex justify-content-center align-items-center">
-                              <li class="caixa-item">
-                                  <a class="caixa-link" href="?page=home">Home</a>
-                              </li>
-                              <li class="caixa-item">
-                                  <a class="caixa-link" href="?page=prestador">Prestador</a>
-                              </li>
-                              <li class="caixa-item">
-                                  <a class="caixa-link" href="?page=busca">Busca</a>
-                              </li>                                                     
-                              <li class="caixa-item">
-                                  <a class="caixa-link" href="?page=avaliacoes">Avaliacoes</a>
-                              </li>
-                              <?php
-                                  if(empty(Session::get('id'))){ 
-                              ?>
-                                  <div class="dropdown">
-                                      <button class="btn btn-outline-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                          Trabalhe Conosco
-                                      </button>
-                                      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                          <li><a class="dropdown-item" href="?page=login">Login</a></li>
-                                          <li><a class="dropdown-item" href="?page=cadastro">Cadastro</a></li>                                        
-                                      </ul>
-                                  </div>                                
-                              <?php 
-                                  }
-                                  else
-                                  { 
-                              ?>
-                                  <div class="dropdown">
-                                      <button class="btn btn-outline-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                          Perfil
-                                      </button>
-                                      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                          <li><a class="dropdown-item" href="?page=config">Configurações</a></li>
-                                          <li><a class="dropdown-item" href="?action=logout">Logout</a></li>                                        
-                                      </ul>
-                                  </div> 
-                              <?php 
-                              }
-                              ?>
-                          </ul>
-                      </div>
-                  </div>
-              </div> 
-          </div>
-        </nav>
-        <div class="barra">            
-        </div>           
-    </header>
+    <?php
+        include_once 'include/header.php';
+    ?>    
     <main class="container">        
         <?php
             if(isset($_GET['page'])){
