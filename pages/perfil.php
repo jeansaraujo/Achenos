51f05cefad8dd33f08685ecbb10eed8e27db5d2f
Jean Elder Santana Araujo <jeanelder.sa@gmail.com>
2022-07-26 11:29:01 -0300

    Continuação de Atualizaçao Cadastral

--- pages/perfil.php
+++ pages/perfil.php
@@ -32,68 +32,33 @@
             <td ><?php echo Session::get('id')?></td>
             <td ><?php echo Session::get('name'); ?></td>
             <td ><?php echo Session::get('email'); ?></td>
-            <td class="d-flex justify-content-around"><a href="?page=cadpess">Atualizar</a></td>
+            <td class="d-flex justify-content-around">
+              <?php
+                if(isset($dadosPessoais[0]['contato1'])){
+                  echo" 
+                    <a href='?page=cadpess&acao=atualizar'>
+                      Atualizar
+                    </a>
+                  ";                  
+                }
+                else{
+                  echo" 
+                    <a href='?page=cadpess&acao=novo'>
+                      Inserir
+                    </a>
+                  ";
+                }
+              ?>
+            </td>
+              
           </tr>
         </table>
-      </div>        
-        <p class="h4 text-end">
-          Dados Pessoais
-        </p>
-        <div style="width=100%">            
-            <table class="table table bordered table-stripted text-center">
-              <thead class="table-light">
-                <tr>
-                  <td>Contato Principal</td>
-                  <td>Tipo Contato</td>
-                  <td>Contato Alternativo</td>
-                  <td>Nascimento</td>
-                  <td>Pais</td>
-                  <td>Estado</td>
-                  <td>Cidade</td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <?php echo $dadosPessoais[0]['contato1'];?>
-                  </td>
-                  <td>
-                    <?php echo $dadosPessoais[0]['tpcontato1'];?>
-                  </td>
-                  <td>
-                    <?php echo $dadosPessoais[0]['contato2'];?>
-                  </td>
-                  <td>
-                    <?php echo $dadosPessoais[0]['birthday'];?>
-                  </td>
-                  <td>
-                    <?php echo $dadosPessoais[0]['pais'];?>
-                  </td>
-                  <td>
-                    <?php echo $dadosPessoais[0]['estado'];?>
-                  </td>
-                  <td>
-                    <?php echo $dadosPessoais[0]['cidade'];?>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <table class="table table bordered table-stripted text-center">
-              <thead class="table-light">
-                <tr>
-                  <td>Descrição Biográfica</td>                
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <?php echo $dadosPessoais[0]['bio'];?>
-                  </td>                  
-                </tr>
-              </tbody>
-            </table>
-        </div>
-
+      </div>
+      <?php
+        if(isset($dadosPessoais[0]['contato1'])){
+          include_once("include/perfil_infopessoal.php");
+        }
+      ?>
     </div>
   </div>
 </div>
